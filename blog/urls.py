f3329398c771e6a722f8b127746275d952c1ed8c
unknown <jastinafa2016@gmail.com>
2024-08-25 22:09:43 +0330

    func sum number

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from webdesign.urls import path
-from blog.views import blog_pages
+from blog.views import blog_pages, list_number, sum_number
 
 
 urlpatterns = [
     path('list/', blog_pages),
+    
+    #get a number for show in url
+    #variable use to path in <> and can use thi varibale in function
+    path('list/<int:number>', list_number),
+    path('list/<slug:number>', sum_number),
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,5 +11,25 @@ def first_blog(request):
     return HttpResponse(text)
 
 
+def list_number(request, number):
+    #The number argument is the same variable that we took(use) in path
+    text = f"""
+    this is list {number}
+    """
+    return HttpResponse(text)
+
+
 def blog_pages(request):
     return HttpResponse("Blog pages: ")
+
+#sum numbers in url
+def sum_number(request, number):
+    numbers = str(number.replace("-", " "))
+    numbers = numbers.split(" ")
+    numbers = (int(number) for number in numbers)
+    new_list = []
+    for number in numbers:
+        new_list.append(number)
+    sum = new_list[0] + new_list[1]
+    return HttpResponse(f" sum : {sum}")
+    
